ab96f4fad2e3be07fb06517367e1c803e630b83b
danielfh4 <hennig.daniel@gmail.com>
2025-06-15 00:37:24 -0300

    Create express-overrides.d.ts

---
+++ server/types/express-overrides.d.ts
@@ -0,0 +1,21 @@
+// server/types/express-overrides.d.ts
+
+import 'express-session'
+import 'express'
+
+declare module 'express-session' {
+  interface SessionData {
+    userId?: string
+  }
+}
+
+declare module 'express' {
+  interface User {
+    id: string
+    // Adicione outros campos que você usa, ex: nome, email
+  }
+
+  interface Request {
+    user?: User
+  }
+}
